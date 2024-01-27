13783a2aaa6495074eddd9dd64e92a6c0e361784
James Burge <james3906@gmail.com>
2024-01-27 15:38:41 +0000

    server working and username binding works correctly

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace ChatServer
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer
 {
     internal class Program
     {
+        static List<Client> _users;
+        static TcpListener _listener;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            _users = new List<Client>();
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 4444);
+            _listener.Start();
+
+            Console.WriteLine("Listening on IP: 127.0.0.1, Port: 4444");
+
+            while(true)
+            {
+                var client = new Client(_listener.AcceptTcpClient());
+                _users.Add(client);
+
+                /* Broadcast the connection to everyone on the server*/
+            }
         }
     }
 }
